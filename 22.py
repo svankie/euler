b48fdada3a869a71ca21c8df9a2ac00d8d97585e
Horacio Bertorello <svankie@gmail.com>
2011-08-19 20:44:24 -0300

    ups.

--- 22.py
+++ 22.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def calculate_total_score(text):
+def calculate_score(text):
     """ project euler #22: calculates the total of all name scores in ´text´. """
     alphabet = '.abcdefghijklmnopqrstuvwxyz'
     corpus = sorted([word.lower().replace('"', '') for word in text.split(',')])
@@ -8,9 +8,7 @@ def calculate_total_score(text):
     for word in corpus:
         wvalue = sum([alphabet.index(c) for c in word]) * (corpus.index(word) + 1)
         vbag.append(wvalue)
-    import ipdb; ipdb.set_trace()
     return sum(vbag)
 
 if __name__ == '__main__':
-    text = open('names.txt').read()
-    print calculate_total_score(text)
+    print calculate_score(open('names.txt').read())
