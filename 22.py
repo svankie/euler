e1f40ea0da947fc814fa244e454087dc2ab5e4f8
Horacio Bertorello <svankie@gmail.com>
2011-08-19 22:59:46 -0300

    list removed.

--- 22.py
+++ 22.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""
+EDIT #1: Get rid of that motherfucking ´vbag´.
+"""
+
 import time
 
 def calculate_score(text):
     """ project euler #22: calculates the total of all name scores in ´text´. """
     alphabet = '.abcdefghijklmnopqrstuvwxyz'
     corpus = sorted([word.lower().replace('"', '') for word in text.split(',')])
-    vbag = []
-    for word in corpus:
-        wvalue = sum([alphabet.index(c) for c in word]) * (corpus.index(word) + 1)
-        vbag.append(wvalue)
-    return sum(vbag)
+    total_score = \
+        sum([sum([alphabet.index(c) for c in word]) * (corpus.index(word) + 1) for word in corpus])
+    return total_score
 
 if __name__ == '__main__':
     text = open('names.txt').read()
     t1 = time.time()
     score = calculate_score(text)
     t2 = time.time()
-    print "The calculation took", t2 - t1, "seconds to run."
+    print "The calculation took", t2 - t1, "seconds to run: score is", score
